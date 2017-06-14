cbe5907f62231605277abed61db3800ecd6653c6
ZloiY <NoRFolf1000@gmail.com>
2017-06-14 17:36:18 +0300

    feat: replace old partials by angular mvc directory structure and now with right angular using

---
+++ assets/src/js/app/todolist/todolist.component.js
@@ -0,0 +1,66 @@
+angular
+.module('todoList')
+.component('todoList', {
+  templateUrl: 'src/js/app/todolist/todolist.template.html',
+  controller:['$scope',
+    function ToDoListHeaderController($scope) {
+      const self = this;
+      $scope.allCheckUncheck = false;
+      $scope.activeElements = 0;
+      $scope.tasks = [];
+      self.addTask = function addTask(taskName) {
+        $scope.task = {};
+        if (taskName.length === 0) {
+          alert('You must entry task name.');
+          return;
+        }
+        const task = $scope.task;
+        $scope.task.name = taskName;
+        $scope.tasks.push(task);
+        $scope.activeElements += 1;
+      };
+      self.closeTask = function closeTask(task) {
+        const index = $scope.tasks.indexOf(task);
+        if (index > -1) {
+          $scope.tasks.splice(index, 1);
+        }
+        !task.check ? $scope.activeElements -= 1 : 0;
+      };
+      self.taskCheck = function taskCheck(task) {
+        task.check ? $scope.activeElements -= 1 : $scope.activeElements += 1;
+      };
+      self.allLabelSort = function allLabelSort() {
+
+      };
+      self.completeLabelSort = function completeLabelSort() {
+
+      };
+      self.activeLabelSort = function activeLabelSort() {
+
+      };
+      self.toggleCheckBtn = function toggleCheckBtn() {
+        angular.forEach($scope.tasks, (task) => {
+           if(!$scope.allCheckUncheck !== task.check) {
+             task.check = !$scope.allCheckUncheck;
+             task.check ? $scope.activeElements -= 1 : $scope.activeElements += 1;
+           }
+        });
+        $scope.allCheckUncheck = !$scope.allCheckUncheck;
+      };
+      self.delCheckedBtn = function delCheckedBtn() {
+        for (let taskId = 0; taskId < $scope.tasks.length; taskId += 1) {
+          const index = $scope.tasks.indexOf($scope.tasks[taskId]);
+          checkTaskForDeleting(index, $scope.tasks[taskId]) ?  $scope.tasks.slice(index, 1) : taskId += 1;
+          taskId -= 1;
+        }
+        $scope.allCheckUncheck = false;
+      }
+    }]
+  });
+
+function checkTaskForDeleting (index, task) {
+  if (task.check && (index > -1)) {
+    return true;
+  }
+  return false;
+}

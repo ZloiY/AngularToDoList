fc04d82462304852fc1450feae9aee5a3e95582c
ZloiY <NoRFolf1000@gmail.com>
2017-06-15 17:57:07 +0300

    refactor: remove unnecessary spaces

--- assets/src/js/app/todolist/todolist.component.spec.js
+++ assets/src/js/app/todolist/todolist.component.spec.js
@@ -1,19 +1,19 @@
 describe('todoList', () => {
   beforeEach(module('todoList'));
   describe('ToDoListController', () => {
-    it('should contain empty input field', inject( ($componentController) => {
+    it('should contain empty input field', inject(($componentController) => {
          const ctrl = $componentController('todoList');
          expect(ctrl.name).toBe('');
          }));
-    it('should contain empty task array', inject( ($componentController) => {
+    it('should contain empty task array', inject(($componentController) => {
          const ctrl = $componentController('todoList');
          expect(ctrl.tasks.length).toBe(0);
         }));
-    it('active task number should equal 0', inject( ($componentController) => {
+    it('active task number should equal 0', inject(($componentController) => {
         const ctrl = $componentController('todoList');
         expect(ctrl.activeElements).toBe(0);
         }));
-    it('allCheckUnchek should be false', inject( ($componentController) => {
+    it('allCheckUnchek should be false', inject(($componentController) => {
       const ctrl = $componentController('todoList');
       expect(ctrl.allCheckUncheck).toBe(false);
     }));
